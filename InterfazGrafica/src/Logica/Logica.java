086483b20e88947564766dcb31be4cf54a68768a
yojhanyac <yac@live.com.ar>
2020-09-09 19:54:08 -0300

    Update

---
+++ InterfazGrafica/src/Logica/Logica.java
@@ -0,0 +1,14 @@
+package Logica;
+
+import IGU.Principal;
+
+public class Logica {
+
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
